d1d34a69ce1fa652b99108d232a74933f8725b9b
Vernie Namca <vernie@mybusybee.net>
2022-11-22 11:41:19 +0800

    Updates on ERC-OFAS Web project architecture
    Updates on ERC-OFAS Web project architecture

---
+++ ERCOFAS.Web/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERC.OFAS.Web.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult SkateHolder()
+        {
+            return View();
+        }
+    }
+}
